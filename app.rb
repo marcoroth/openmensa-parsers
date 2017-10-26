dec68e445f5550336da8e6d588bba2587c9efaf1
Marco Roth <marco.roth@intergga.ch>
2017-10-26 10:14:27 +0200

    change index meta from xml to json

--- app.rb
+++ app.rb
@@ -15,8 +15,24 @@ get '/parsers/svgroup' do
   erb :index
 end
 
-get '/parsers/svgroup/meta', provides: ['xml'] do
-  nokogiri :index
+get '/parsers/svgroup/meta', provides: ['json'] do
+  url = "http://mensa-fhnw.sv-restaurant.ch/de/menuplan/persrest-data.json"
+  json = JSON.load(open(url))
+
+  canteens = []
+
+  json["items"].each do |i|
+    begin
+      uri = URI(i["link"])
+      host = uri.host
+      mensa_name = host.split(".")[0]
+
+      canteens << [ mensa_name, "#{request.scheme}://" + request.host + ":" + request.port.to_s + "/parsers/svgroup/" + mensa_name + "/meta" ]
+
+    rescue
+    end
+  end
+  canteens.to_json
 end
 
 get '/parsers/svgroup/:name' do
